4f31dc0d721d5799159d76249d2246526ab8d45e
Your Name <you@example.com>
2018-02-23 15:05:22 +0530

    deploy file changed

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ $ENRONMENT = "QA" ]; then
 
    sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
 
-eiif [ $ENVIRONMENT = "TEST" ]; then
+elif [ $ENVIRONMENT = "TEST" ]; then
 
     sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
 
