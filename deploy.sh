79cfb300aee73c075a70902a2eb83a32e8ec94ed
Your Name <you@example.com>
2018-02-23 14:42:14 +0530

    deploy changed

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,35 @@ if [ $ENRONMENT = "QA" ]; then
 #
 # this deploys the war file into tomcat's webapps directory on Slave1
 
-sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/practice/apache-tomcat-9.0.5/webapps
+	sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/practice/apache-tomcat-9.0.5/webapps
 #
 # this login to tomcat machine set java environment and start the tomcat server on Slave1
 
-sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
+	sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "TEST" ]; then
 #
 # this deploys the war file into tomcat's webapps directory on Slave2
 
-sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
+	sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
 #
 # this login to tomcat machine set java environment and start the tomcat server on Slave2
 
-sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk-9.0.4" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+	sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk-9.0.4" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+
+
+
+elif [ $ENVIRONMENT = "ALL" ]; then
+
+	sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/practice/apache-tomcat-9.0.5/webapps
+
+	sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
+
+	sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
+
+	sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk-9.0.4" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+
+fi
 
 fi
 
