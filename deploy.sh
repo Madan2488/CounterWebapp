75803ed584e70297bb389ca838477bb3596778b7
Your Name <you@example.com>
2018-02-28 23:19:42 +0530

    deploy.sh file modified

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,25 @@
 
 if [ $ENVIRONMENT = "QA" ]; then
 
-   sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/practice/apache-tomcat-9.0.5/webapps
+   sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
-   sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
+   sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "TEST" ]; then
 
     sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
 
-    sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk-9.0.4" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+    sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "ALL" ]; then
 
-    sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/practice/apache-tomcat-9.0.5/webapps
+    sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
-    sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/practice/jdk-9.0.4" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
+    sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
     sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
 
-    sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk-9.0.4" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+    sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 fi
 
