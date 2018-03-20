4dbc6cd0838c7795621adf76042458e4510b168c
Your Name <you@example.com>
2018-03-20 12:56:41 +0530

    deply.sh changes

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,23 @@
 
 if [ $ENVIRONMENT = "QA" ]; then
 
-sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.jar root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
 sshpass -p "root" ssh root@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "TEST" ]; then
 
-sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.jar root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
 
 sshpass -p "root" ssh root@172.17.0.4 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "ALL" ]; then
 
-sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.jar root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
 sshpass -p "root" ssh root@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
-sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.jar root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
 
 sshpass -p "root" ssh root@172.17.0.4 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
