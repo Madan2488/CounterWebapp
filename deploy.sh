6f6eb625fc00bc106e8fa48179eb26534eb2868d
Your Name <you@example.com>
2018-03-20 12:23:56 +0530

    deply.sh changes

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,25 @@
 
 if [ $ENVIRONMENT = "QA" ]; then
 
-sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.3:/home/practice/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
-sshpass -p "root" ssh root@172.17.0.3 "JAVA_HOME=/home/practice/jdk1.8.0_161" "/home/practice/apache-tomcat-9.0.5/bin/startup.sh"
+sshpass -p "root" ssh root@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "TEST" ]; then
 
-sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
 
-sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+sshpass -p "root" ssh root@172.17.0.4 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 elif [ $ENVIRONMENT = "ALL" ]; then
 
-sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.2:/home/Distors/apache-tomcat-9.0.5/webapps
 
-sshpass -p "charan" ssh charan@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+sshpass -p "root" ssh root@172.17.0.2 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
-sshpass -p "charan" scp $WORKSPACE/target/CounterWebApp.war charan@172.17.0.3:/home/Distors/apache-tomcat-9.0.5/webapps
+sshpass -p "root" scp $WORKSPACE/target/CounterWebApp.war root@172.17.0.4:/home/Distors/apache-tomcat-9.0.5/webapps
 
-sshpass -p "charan" ssh charan@172.17.0.3 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
+sshpass -p "root" ssh root@172.17.0.4 "JAVA_HOME=/home/Distors/jdk1.8.0_161" "/home/Distors/apache-tomcat-9.0.5/bin/startup.sh"
 
 fi
 
